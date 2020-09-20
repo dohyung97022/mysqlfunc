64a4031e68e8147c3dea64fbf7ef09177f4d77b8
Doe <dohyung97022@gmail.com>
2020-09-20 11:04:21 +0900

    .

--- commit_test.bat
+++ commit_test.bat
@@ -1,16 +1,16 @@
 :: allways check if password is deleted before running this bat file!
+@echo off
 
 :LOOP
-set /p YN=(Y/N)?
+set /p YN=Did you delete your password in the test file? (YES/NO)?
 
-if /i "%YN%" == "y" goto YES
-if /i "%YN%" == "n" goto NO
+if /i "%YN%" == "yes" goto YES
+if /i "%YN%" == "no" goto NO
 
 goto LOOP
 
 
 :YES
-echo YES
 git update-index --no--assume-unchanged "mysqlfunc_test.go"
 git add .
 git commit -m "."
@@ -18,7 +18,7 @@ git push origin master
 git update-index --assume-unchanged "mysqlfunc_test.go"
 
 :NO
-echo NO
+
 goto QUIT
 
 :QUIT
