5206a9ee6b74c51f330a92081cc09333a1b0a85d
Doe <dohyung97022@gmail.com>
2020-09-20 10:54:06 +0900

    .

--- commit.bat
+++ commit.bat
@@ -1,5 +1,6 @@
 git config --global user.email "dohyung97022@gmail.com"
 git config --global user.name "Doe"
+git update-index --assume-unchanged "mysqlfunc_test.go"
 git add .
 git commit -m "."
 git push origin master
